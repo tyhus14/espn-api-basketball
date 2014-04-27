a61d8a572f180d7f48d8483e371a6fdcedc7670c
tyhus14 <huston.tyler3@gmail.com>
2014-04-27 13:45:30 -0400

    updated scroll and media queries

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	router = new MainRouter();
+    router = new MainRouter();
 
-	Backbone.history.start(); 
+    Backbone.history.start();
+
 
 });
+
+$.fn.scrollView = function() {
+    return this.each(function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top
+        }, 1500);
+    });
+}
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,30 +1,31 @@
 var MainRouter = Backbone.Router.extend({
-  routes: {
-    ':id' : 'featuredTeam'
-  },
- 
-  initialize: function(){
-    this.items = new TeamCollection();
-    this.headlines = new HeadlineCollection()
-    this.items.fetch();
-  },
- 
-  featuredTeam: function(id){
-    console.log('hi')
-    // if (id) {
-      // this.items.url = "http://api.espn.com/v1/sports/basketball/nba/teams/"+ id +"/news?&apikey=swz92eu339qnv7tsd8ypqpxq"
-      this.headlines.url = "http://api.espn.com/v1/sports/basketball/nba/teams/"+ id +"/news?&apikey=swz92eu339qnv7tsd8ypqpxq"
-    // }
+    routes: {
+        ':id': 'featuredTeam'
+    },
 
-    this.headlines.fetch()
+    initialize: function() {
+        this.items = new TeamCollection();
+        this.headlines = new HeadlineCollection()
+        this.items.fetch();
+    },
 
-    $('.featured-list').html('')
+    featuredTeam: function(id) {
+        console.log('hi')
+        // if (id) {
+        // this.items.url = "http://api.espn.com/v1/sports/basketball/nba/teams/"+ id +"/news?&apikey=swz92eu339qnv7tsd8ypqpxq"
+        this.headlines.url = "http://api.espn.com/v1/sports/basketball/nba/teams/" + id + "/news?&apikey=swz92eu339qnv7tsd8ypqpxq"
+        // }
 
-    console.log('id is: ', id)
-    // new FeaturedView({model: )
+        this.headlines.fetch()
 
+        $('.featured-list').html('')
 
-  }
+        console.log('id is: ', id)
+        // new FeaturedView({model: )
+
+        $('.featured-list').scrollView();
+
+    }
 
 
 });
